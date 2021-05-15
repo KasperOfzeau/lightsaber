6e1b42ea5090d0fbc0aad14bc0104609560523a9
Kasper Beljaars <kasperbeljaars@hotmail.com>
2021-05-15 22:44:33 +0200

    Added js mute button

--- js/main.js
+++ js/main.js
@@ -3,12 +3,15 @@ const powerDown = new Audio('sounds/powerDown.mp3');
 const hilt = document.querySelector('.hilt');
 const lightsaber = document.querySelector('.lightsaber');
 const colorSelecter = document.querySelector('#colors');
+const muteButton = document.querySelector('#muteButton');
+let volume = 0.2;
 
 const init = () => {
     hilt.addEventListener('click', playIgnite);
     colorSelecter.addEventListener('change', colorSelect);
-    igniteSound.volume = 0.2;
-    powerDown.volume = 0.2;
+    muteButton.addEventListener('click', changeVolume);
+    igniteSound.volume = volume;
+    powerDown.volume = volume;
 }
 
 const playIgnite = () => {
@@ -25,6 +28,24 @@ const playPowerDown = () => {
 
 const colorSelect = (e) => {
     lightsaber.id =  e.target.value;
+}
+
+const changeVolume = () => {
+    if(volume == 0.2) {
+        volume = 0;
+        igniteSound.volume = volume;
+        powerDown.volume = volume;
+
+        muteButton.classList = "";
+        muteButton.classList = "fas fa-volume-mute";
+    } else {
+        volume = 0.2;
+        igniteSound.volume = volume;
+        powerDown.volume = volume;
+
+        muteButton.classList = "";
+        muteButton.classList = "fas fa-volume-up";
+    }
 }
 
 init();
