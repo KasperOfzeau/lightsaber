87db04958ecbaffc6bbdc35229df294f5522c4b5
Kasper Beljaars <kasperbeljaars@hotmail.com>
2021-05-15 22:24:02 +0200

    Added JS for color lightsaber from select box

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 const igniteSound = new Audio('sounds/igniting.mp3');
 const powerDown = new Audio('sounds/powerDown.mp3');
 const hilt = document.querySelector('.hilt');
+const lightsaber = document.querySelector('.lightsaber');
+const colorSelecter = document.querySelector('#colors');
 
 const init = () => {
     hilt.addEventListener('click', playIgnite);
+    colorSelecter.addEventListener('change', colorSelect);
     igniteSound.volume = 0.2;
     powerDown.volume = 0.2;
 }
@@ -18,6 +21,10 @@ const playPowerDown = () => {
     powerDown.play();
     hilt.removeEventListener('click', playPowerDown);
     hilt.addEventListener('click', playIgnite);
+}
+
+const colorSelect = (e) => {
+    lightsaber.id =  e.target.value;
 }
 
 init();
